76e2915d755120e7ec453162dfcc7f4b2f27ba43
Ashok <kumarashok60951@gmail.com>
2025-05-17 23:49:15 +0530

    functions implemented

--- script.js
+++ script.js
@@ -8,20 +8,42 @@ let arr = [
 
 function PrintDeveloperbyMap() {
   //Write your code here , just console.log
+ const developers = arr
+  .filter(person => person.profession === "developer")
+  .map(dev => {
+    console.log(dev); // print each developer object
+    return dev;
+  });  
+ 
 }
 
 function PrintDeveloperbyForEach() {
   //Write your code here , just console.log
+  arr.filter(person => person.profession === "developer").forEach(developer => console.log(developer));
 }
 
 function addData() {
   //Write your code here, just console.log
+  let newperson =  {id:4,name:"susan",age:"20",profession:"intern"};
+  arr.push(newperson);
+  console.log(arr);  
 }
 
 function removeAdmin() {
   //Write your code here, just console.log
+  arr = arr.filter(person => person.profession !== "admin");
+  console.log(arr);
+  
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  let arr2 = [
+  { id: 4, name: "emma", age: "24", profession: "designer" },
+  { id: 5, name: "mike", age: "29", profession: "manager" },
+  { id: 6, name: "lisa", age: "23", profession: "tester" }
+  ];
+  let combine = arr.concat(arr2);
+  console.log(combine);
+  
 }
